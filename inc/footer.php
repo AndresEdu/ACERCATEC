6b41d2f3db93436bf12547939ca31b1a47e9540c
AndresEdu <andreseduarci@gmail.com>
2023-10-10 10:46:42 -0600

    Quito las cosas que puso liz

--- inc/footer.php
+++ inc/footer.php
@@ -87,7 +87,7 @@
 
     <!-- Grid container -->
 
-    <!-- Copyright  c       l   iz es genial-->
+    <!-- Copyright-->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
         ASERCATEC
     </div>
