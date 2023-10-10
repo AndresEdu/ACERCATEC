fef2942031b104651a0d4e4ad8eee119878f1dc4
AndresEdu <andreseduarci@gmail.com>
2023-10-10 10:45:21 -0600

    Cambio C de ASERCATEC

--- inc/footer.php
+++ inc/footer.php
@@ -89,7 +89,7 @@
 
     <!-- Copyright  c       l   iz es genial-->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        ACERCATEC
+        ASERCATEC
     </div>
     <!-- Copyright -->
 </footer>
